633ce6c6c899b59b525d2cbf4a8c23c1adc721ab
Dorek <dorota@MacBook-Pro.local>
2017-08-07 14:31:20 -0400

    fixed leading '_' problem if the string starts with a capital letter

--- src/main/scala/io/glassdome/translate/SnakeTranslator.scala
+++ src/main/scala/io/glassdome/translate/SnakeTranslator.scala
@@ -7,9 +7,15 @@ package io.glassdome.translate
 object SnakeTranslator extends Translator {
 
   def translate(s: String, mapping: Map[String, String] = Map()): String = {
-    s.foldLeft("") { (acc, c) =>
+    val result = s.foldLeft("") { (acc, c) =>
       if (c.isLower) acc + c else acc + s"_${c.toLower}"
     }
+
+    //drops '_' that was created  if the first letter was upper case
+    if (result.charAt(0) == '_') result.drop(1)
+    else result
   }
-  
+
+
 }
+
